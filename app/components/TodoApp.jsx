626f7bfcf5d414d5447aa8ad41bd755ef24e2e19
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2017-04-16 00:57:15 -0400

    testing with static todos

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,11 +1,37 @@
 let React = require("react");
+let TodoList = require("TodoList");
 
 let TodoApp = React.createClass({
+    getInitialState: function() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: "First todo"
+                },
+                {
+                    id: 2,
+                    text: "Second todo"
+                },
+                {
+                    id: 3,
+                    text: "Third todo"
+                },
+                {
+                    id: 4,
+                    text: "Fourth todo"
+                }
+            ]
+        };
 
+    },
     render: function () {
+        let {todos} = this.state;
+
+
         return (
             <div>
-                <p>TodoApp.jsx</p>
+                <TodoList todos={todos}/>
             </div>
         );
     }
